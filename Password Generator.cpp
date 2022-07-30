06a6eae449dc1f05522e4a7acb6b83600d6b870c
Milena T <86500701+jirkawq@users.noreply.github.com>
2022-07-29 21:08:09 -0300

    Update Password Generator.cpp

--- Password Generator.cpp
+++ Password Generator.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
-#include <string.h>
 
-constexpr int N = 50;
+constexpr int N = 100;
 int main()
 {
 	const char password_characters[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int password_length;
-
-	std::cout << "Password length [Range of 8-16]: ";
-	std::cin >> password_length;
+	do {
+		std::cout << "Password length [Range of 8-16]: ";
+		std::cin >> password_length;
+	} while (password_length < 8 || password_length > 16);
 
 	if (password_length >= 8 && password_length <= 16) {
-		system("cls");
-		std::cout << "Generated password: ";
-		
+		std::cout << "Generated Password: ";
+
 		for (int i = 0; i < password_length; i++) {
-			std::cout << password_characters[rand() % sizeof(password_characters) - 1];
+			std::cout << password_characters[rand() % sizeof(password_characters)];
 		}
+		std::cout << "\n\n";
+		system("pause");
+		return 0;
 	}
-	
-	else {
-		std::cout << "That number is not in the range.";
-	}
-	std::cout << "\n\n";
-	system("pause");
-	return 0;
 }
